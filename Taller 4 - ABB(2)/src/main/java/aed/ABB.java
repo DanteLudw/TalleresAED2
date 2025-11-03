a260ae8dd650cfa09f7c9f1cb12caf9a36769c4e
DanteLudw <dpolicanoura@gmail.com>
2025-11-03 15:48:05 -0300

    Implement methods for minimum, maximum, and insertion

--- Taller 4 - ABB(2)/src/main/java/aed/ABB.java
+++ Taller 4 - ABB(2)/src/main/java/aed/ABB.java
@@ -1,9 +1,8 @@
 package aed;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
-// Todos los tipos de datos "Comparables" tienen el método compareTo()
-// elem1.compareTo(elem2) devuelve un entero. Si es mayor a 0, entonces elem1 > elem2
 public class ABB<T extends Comparable<T>> {
     private Nodo raiz ;
     private int cardinal;
@@ -33,72 +32,174 @@ public class ABB<T extends Comparable<T>> {
     }
 
     public T minimo(){
-        throw new UnsupportedOperationException("No implementada aun");
+        if (raiz == null) return null;
+        Nodo actual = raiz;
+        while (actual.izq != null) {
+            actual = actual.izq;
+        }
+        return actual.valor;
     }
+    
 
     public T maximo(){
-        throw new UnsupportedOperationException("No implementada aun");
+        if (raiz == null) return null;
+        Nodo actual = raiz;
+        while (actual.der != null) {
+            actual = actual.der;
+        }
+        return actual.valor;
     }
 
-    public void insertar(T elem){
-        if (raiz==null){
-            raiz.valor = elem ;
-            altura=1;
-        }
-        int alturaPrima=0;
-        funcionAux(raiz, elem, alturaPrima);
-        if (alturaPrima > altura){
-            altura=alturaPrima;
+    public void insertar(T elem) {
+    if (raiz == null) {
+        raiz = new Nodo(elem);
+        cardinal = 1;
+        altura = 1;
+    } else {
+        int nuevaAltura = insertarRecursivo(raiz, elem, 1);
+        if (nuevaAltura > altura) {
+            altura = nuevaAltura;
         }
     }
-
-    private funcionAux( Nodo raiz, T elem, int: altura) ->Nodo nuevo, alturaPrima{
-        Nodo nuevo;
-        alturaPrima=altura+1;
-        if (elem>raiz.valor){
-            if (raiz.der==null){
-                raiz.der=Nuevo;
-            }
-            else{
-                funcionAux(raiz.der,elem,alturaPrima);
-            }
+}
+private int insertarRecursivo(Nodo actual, T elem, int alt) {
+    int comparacion = elem.compareTo(actual.valor);
+    if (comparacion == 0) {
+        return alt;
+    } else if (comparacion < 0) {
+        if (actual.izq == null) {
+            actual.izq = new Nodo(elem);
+            actual.izq.padre = actual;
+            cardinal++;
+            return alt + 1;
+        } else {
+            return insertarRecursivo(actual.izq, elem, alt + 1);
+        }
+    } else {
+        if (actual.der == null) {
+            actual.der = new Nodo(elem);
+            actual.der.padre = actual;
+            cardinal++;
+            return alt + 1;
+        } else {
+            return insertarRecursivo(actual.der, elem, alt + 1);
+        }
+    }
+}
+    public boolean pertenece(T elem) {
+        Nodo actual = raiz;
+        while (actual != null) {
+            int cmp = elem.compareTo(actual.valor);
+            if (cmp == 0) return true;
+            actual = (cmp < 0) ? actual.izq : actual.der;
+        }
+        return false;
+    }
+public void eliminar(T elem) {
+        Nodo nodo = buscarNodo(elem);
+        if (nodo == null) return;
+        cardinal--;
+        if ( nodo.izq==null && nodo.der==null){
+            reemplazarEnPadre(nodo, null);
+        }
+        else if ( nodo.izq!=null && nodo.der==null){
+            reemplazarEnPadre(nodo, nodo.izq);
+        }
+        else if ( nodo.izq==null && nodo.der!=null){
+            reemplazarEnPadre(nodo, nodo.der);
         }
         else {
-            if (raiz.izq==null){
-                raiz.izq=Nuevo;
-            }
-            else{
-                funcionAux(raiz.izq,elem,alturaPrima);
-            }
-        }
-    }
-    public boolean pertenece(T elem){
-        if (raiz==null ) {
-            return false;
-        }
-
-    }
-
-    public void eliminar(T elem){
-        throw new UnsupportedOperationException("No implementada aun");
-    }
-
-    public String toString(){
-        throw new UnsupportedOperationException("No implementada aun");
-    }
-
-    public class ABB_Iterador {
-        private Nodo _actual;
-
-        public boolean haySiguiente() {            
-            throw new UnsupportedOperationException("No implementada aun");
-        }
-    
-        public T siguiente() {
-            throw new UnsupportedOperationException("No implementada aun");
+            Nodo sucesor = minimoNodo(nodo.der);
+            nodo.valor = sucesor.valor;
+            eliminarNodo(sucesor);
         }
     }
 
+    private Nodo buscarNodo(T elem) {
+        Nodo actual = raiz;
+        while (actual != null) {
+            int cmp = elem.compareTo(actual.valor);
+            if (cmp == 0) return actual;
+            actual = (cmp < 0) ? actual.izq : actual.der;
+        }
+        return null;
+    }
+
+    private Nodo minimoNodo(Nodo nodo) {
+        while (nodo.izq != null) {
+            nodo= nodo.izq;
+        }
+        return nodo;
+    }
+
+    private void eliminarNodo(Nodo nodo) {
+        if (nodo.izq == null && nodo.der == null) {
+            reemplazarEnPadre(nodo, null);
+        } else if (nodo.izq != null && nodo.der == null) {
+            reemplazarEnPadre(nodo, nodo.izq);
+        } else {
+            reemplazarEnPadre(nodo, nodo.der);
+        }
+    }
+
+    private void reemplazarEnPadre(Nodo nodo, Nodo nuevo) {
+        if (nodo.padre ==null){
+            raiz=nuevo;
+        }
+        else if (nodo == nodo.padre.izq) {
+            nodo.padre.izq= nuevo ;
+        }
+        else{
+            nodo.padre.der = nuevo;
+        }
+        if (nuevo != null) {
+            nuevo.padre = nodo.padre;
+        }
+    }
+       
+   @Override
+    public String toString() {
+        List<String> elementos = new ArrayList<>();
+        recorridoInOrder(raiz, elementos);
+        return "{" + String.join(",", elementos) + "}";
+    }
+
+    private void recorridoInOrder(Nodo actual, List<String> lista) {
+        if (actual == null) return;
+
+        recorridoInOrder(actual.izq, lista);                  
+        lista.add(actual.valor.toString());                  
+        recorridoInOrder(actual.der, lista);                 
+    }
+
+
+   public class ABB_Iterador {
+    private List<T> elementos;
+    private int posicion;
+
+    public ABB_Iterador() {
+        elementos = new ArrayList<>();
+        posicion = 0;
+        recorridoInOrder(raiz); 
+    }
+
+    private void recorridoInOrder(Nodo actual) {
+        if (actual == null) return;
+        recorridoInOrder(actual.izq);
+        elementos.add(actual.valor);
+        recorridoInOrder(actual.der);
+    }
+
+    public boolean haySiguiente() {
+        return posicion < elementos.size();
+    }
+
+    public T siguiente() {
+        T valor = elementos.get(posicion);
+        posicion++;
+        return valor;
+    }
+}
     public ABB_Iterador iterador() {
         return new ABB_Iterador();
     }
